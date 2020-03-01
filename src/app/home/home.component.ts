c2a9fdcc86ae21cb49a2f5da328874434a024a71
bharathck <bharathchandra541@gmail.com>
2020-03-01 23:43:47 +0530

    'homeComponent'

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  recentArray = [1,2,3,4,5,6,7];
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+}
