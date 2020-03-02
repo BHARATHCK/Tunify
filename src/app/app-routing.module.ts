e491756da23bba0a053482d8ea1c50d5ee84145e
bharath chandra <bharathchandra541@gmail.com>
2020-03-02 17:53:03 +0530

    'Seperate_Routing_for_Desktop_and_Mobile'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { MyplaylistsComponent } from './myplaylists/myplaylists.component';
 import { RecentComponent } from './recent/recent.component';
 import { HomeComponent } from './home/home.component';
+import { ArtistsComponent } from './mobileViews/artists/artists.component';
+import { TracksComponent } from './mobileViews/tracks/tracks.component';
+import { RecentlyPlayedComponent } from './mobileViews/recently-played/recently-played.component'
 
-const routes: Routes = [
+const desktop_routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'home' , component: HomeComponent},
   {path: 'recent' , component: RecentComponent},
   {path: 'myplaylist' , component: MyplaylistsComponent}
 ];
 
+const mobile_routes: Routes = [
+  {path: '' , component: ArtistsComponent},
+  {path: 'home' , component: ArtistsComponent},
+  {path: 'recent' , component: RecentlyPlayedComponent},
+  {path: 'myplaylist' , component: MyplaylistsComponent}
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(desktop_routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
