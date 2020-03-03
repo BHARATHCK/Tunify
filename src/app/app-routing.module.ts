f1a1c67fde9767aef601e1c3f5ee40c5bbda3c65
bharath chandra <bharathchandra541@gmail.com>
2020-03-03 11:14:35 +0530

    'change_routes_on_the_go'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,58 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 import { MyplaylistsComponent } from './myplaylists/myplaylists.component';
 import { RecentComponent } from './recent/recent.component';
 import { HomeComponent } from './home/home.component';
 import { ArtistsComponent } from './mobileViews/artists/artists.component';
 import { TracksComponent } from './mobileViews/tracks/tracks.component';
-import { RecentlyPlayedComponent } from './mobileViews/recently-played/recently-played.component'
+import { RecentlyPlayedComponent } from './mobileViews/recently-played/recently-played.component';
+import { ApplicationStateService } from './services/application-state/application-state.service';
 
 const desktop_routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'home' , component: HomeComponent},
   {path: 'recent' , component: RecentComponent},
-  {path: 'myplaylist' , component: MyplaylistsComponent}
+  {path: 'myplaylist' , component: MyplaylistsComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 const mobile_routes: Routes = [
   {path: '' , component: ArtistsComponent},
   {path: 'home' , component: ArtistsComponent},
   {path: 'recent' , component: RecentlyPlayedComponent},
-  {path: 'myplaylist' , component: MyplaylistsComponent}
+  {path: 'myplaylist' , component: MyplaylistsComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(desktop_routes)],
+  imports: [RouterModule.forRoot(desktop_routes , {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+
+  constructor(private router: Router , private applicationStateService: ApplicationStateService){
+    this.routeBasedOnResChange();
+  }
+
+
+
+  routeBasedOnResChange(){
+    this.applicationStateService.resChange.subscribe(
+      res => {
+        if(res){
+          this.router.resetConfig(mobile_routes);
+          
+          this.router.navigate([this.router.url]);
+
+        } else {
+          this.router.resetConfig(desktop_routes);
+
+          this.router.navigate([this.router.url]);
+        }
+      }
+    )
+  }
+
+
+
+}
