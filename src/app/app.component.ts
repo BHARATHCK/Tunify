20e8dceef40c74ccb7b49773256b2488b3537797
bharath chandra <bharathchandra541@gmail.com>
2020-03-05 18:16:45 +0530

    'Previous_commit_continue'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener } from '@angular/core';  
-import { ApplicationStateService} from './services/application-state/application-state.service';
+import { Component, HostListener } from '@angular/core';
+import { ApplicationStateService } from './services/application-state/application-state.service';
 
 
 @Component({
@@ -7,45 +7,51 @@ import { ApplicationStateService} from './services/application-state/application
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
   title = 'bharathck';
 
   constructor(private applicationStateService: ApplicationStateService) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.HandleResolutionChange();
-   }
-
-   HandleResolutionChange(){
-     if(window.innerWidth < 768){
-       this.applicationStateService.resChange.next(true);
-     } else {
-      this.applicationStateService.resChange.next(false);
-     }
-   }
-
-   onResizeF(){
-     console.log('TIMEOUT FUNCTION TRIGGERRED ME');
-    this.applicationStateService.notifyWindowResize(window.innerWidth);
-    if(window.innerWidth < 768){
-     // trigger mobile routes
-     let isMobile:Boolean = true;
-     this.applicationStateService.handleResolutionChange(isMobile);
-   } else {
-    let isMobile:Boolean = false;
-     this.applicationStateService.handleResolutionChange(isMobile);
-   }
-   }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event){
-
-   let triggerResize;
-   triggerResize = setTimeout(this.onResize, 400);
-   window.onresize = function(){
-     clearTimeout(triggerResize);
-     triggerResize = setTimeout(this.onResize, 400);
-   }
   }
 
+  HandleResolutionChange() {
+    if (window.innerWidth < 768) {
+      this.applicationStateService.resChange.next(true);
+    } else {
+      this.applicationStateService.resChange.next(false);
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  @debounce()
+  onResize(event) {
+    this.applicationStateService.notifyWindowResize(event.target.innerWidth);
+    if (event.target.innerWidth < 768) {
+      // trigger mobile routes
+      let isMobile: Boolean = true;
+      this.applicationStateService.handleResolutionChange(isMobile);
+    } else {
+      let isMobile: Boolean = false;
+      this.applicationStateService.handleResolutionChange(isMobile);
+    }
+  }
+
+}
+
+
+export function debounce(delay: number = 500): MethodDecorator {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const timeoutKey = Symbol();
+
+    const original = descriptor.value;
+
+    descriptor.value = function (...args) {
+      clearTimeout(this[timeoutKey]);
+      this[timeoutKey] = setTimeout(() => original.apply(this, args), delay);
+    ***REMOVED***
+
+    return descriptor;
+  ***REMOVED***
 }
