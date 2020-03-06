ae67257d34f371e3b3018c833affb1fca10bbfa0
bharath chandra <bharathchandra541@gmail.com>
2020-03-06 17:22:30 +0530

    'Authorization_Service

--- src/app/services/spotify-service/spotify.service.ts
+++ src/app/services/spotify-service/spotify.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import * as qs from 'qs';
+import * as uuid from 'uuid';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: Router) { }
+
+  Login(showDialog = false /* FOR NOW */){
+    const state = uuid(); 
+    localStorage.setItem('xsrf-token', state)
+    const params = {
+      response_type: 'token',
+      client_id: `${environment.clientId}`,
+      redirect_uri: `${environment.redirectUrl}`,
+      scope: `${environment.scopes}`,
+        // tslint:disable-next-line: max-line-length,
+      state,
+      show_dialog: `${showDialog}`
+    }
+    const redirectUrl = `${environment.appRedirectUrl}+${qs.stringify(params)}`;
+    window.location.href = redirectUrl;
+  } 
 }
