02db437ddbad033469d148b1f33e0803c07258a2
bharath chandra <bharathchandra541@gmail.com>
2020-03-06 16:44:22 +0530

    'clean

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,27 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  /*
-  firebase: {
-    apiKey: 'xxx',
-    authDomain: 'gkz-angular-firebase.firebaseapp.com',
-    databaseURL: 'https://gkz-angular-firebase.firebaseio.com',
-    projectId: 'gkz-angular-firebase',
-    storageBucket: 'gkz-angular-firebase.appspot.com',
-    messagingSenderId: '123'
-  }
-  */
-  firebase: {
-    apiKey: "AIzaSyAzhheY45Wrq4YXGhp7xrnH-QXlLSwhxos",
-    authDomain: "blog-bharathck.firebaseapp.com",
-    databaseURL: "https://blog-bharathck.firebaseio.com",
-    projectId: "blog-bharathck",
-    storageBucket: "blog-bharathck.appspot.com",
-    messagingSenderId: "289436804465",
-    appId: "1:289436804465:web:c2addb609bb31da52d02d0",
-    measurementId: "G-48NY9B62F0"
-  }
 };
 
 /*
