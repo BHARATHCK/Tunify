191ceb8b25023319f7aa97b2f28c3670fcade8bd
bharathck <bharathchandra541@gmail.com>
2020-03-07 10:56:56 +0530

    'spotify_Service'

--- src/app/services/spotify-service/spotify.service.ts
+++ src/app/services/spotify-service/spotify.service.ts
@@ -4,15 +4,18 @@ import { environment } from '../../../environments/environment';
 import * as qs from 'qs';
 import * as uuid from 'uuid';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { CacheRegistrationService } from '../cacheservice/cache-registration.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor(private http: HttpClient, private route: Router) { }
+  constructor(private http: HttpClient, private route: Router, private cacheRegistrationService: CacheRegistrationService) { }
 
-  Login(showDialog = false /* FOR NOW */){
+  Login(showDialog = true /* FOR NOW */){
     const state = uuid(); 
     localStorage.setItem('xsrf-token', state)
     const params = {
@@ -20,11 +23,25 @@ export class SpotifyService {
       client_id: `${environment.clientId}`,
       redirect_uri: `${environment.redirectUrl}`,
       scope: `${environment.scopes}`,
-        // tslint:disable-next-line: max-line-length,
       state,
       show_dialog: `${showDialog}`
     }
-    const redirectUrl = `${environment.appRedirectUrl}+${qs.stringify(params)}`;
+    const redirectUrl = `${environment.appRedirectUrl}${qs.stringify(params)}`;
+    console.log(redirectUrl);
     window.location.href = redirectUrl;
-  } 
+  }
+
+  refreshLogin(){
+    this.Login(false);
+  }
+
+  getToken(): String{
+    return localStorage.getItem('access_token');
+  }
+
+  getRecentTracksForHome(): Observable<any> {
+    const baseURL = 'https://api.spotify.com/v1/me/player/recently-played?limit=7';
+    this.cacheRegistrationService.addToCache(baseURL);
+    return this.http.get<any>(baseURL).pipe(shareReplay(1));
+  }
 }
