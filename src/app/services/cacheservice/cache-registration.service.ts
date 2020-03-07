487b22ba25347efad188bf4d7b517a6b8620e3f9
bharathck <bharathchandra541@gmail.com>
2020-03-07 11:03:08 +0530

    'cache_registration_service'

---
+++ src/app/services/cacheservice/cache-registration.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheRegistrationService {
+
+  constructor() { }
+
+  private services = [];
+
+	public addedToCache(serviceUri: string) {
+		return this.services.indexOf(serviceUri) > -1;
+	}
+
+	public addToCache(serviceUri: string) {
+		// Check if not already added to list
+		if (!this.addedToCache(serviceUri)) {
+			this.services.push(serviceUri);
+		}
+  }
+  
+}
