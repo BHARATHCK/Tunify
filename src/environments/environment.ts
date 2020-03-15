85e9da8ea58980bebf4767ad3df876d21639a481
bharathck <bharathchandra541@gmail.com>
2020-03-15 21:47:33 +0530

    username greeting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-***REMOVED***
-***REMOVED***
+export const environment = {
+    production: true,
+    clientId: '***REMOVED***',
+    redirectUrl: 'http://localhost:4200',
+    scopes: '***REMOVED***',
+    appRedirectUrl: '***REMOVED***'
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
