45b81a942bf6cbf501b153fe96a96ebdc4e070ad
bharathck <bharathchandra541@gmail.com>
2020-03-19 22:00:58 +0530

    playlist hook new feature

--- src/app/playlist-hook/playlist-hook.component.ts
+++ src/app/playlist-hook/playlist-hook.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SpotifyService } from '../services/spotify-service/spotify.service';
+import { ApplicationStateService } from '../services/application-state/application-state.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-playlist-hook',
@@ -10,8 +12,12 @@ import { SpotifyService } from '../services/spotify-service/spotify.service';
 export class PlaylistHookComponent implements OnInit {
 
   tracks;
+  playlistName;
 
-  constructor(private router: Router, private spotifyService: SpotifyService) { 
+  validateForm: FormGroup;
+  isformdirty: boolean = false;
+
+  constructor(private router: Router, private spotifyService: SpotifyService, private appStateService: ApplicationStateService, private fb: FormBuilder) { 
     let data = this.router.getCurrentNavigation().extras.state;
     if(data){
       this.tracks = data.data.playlist;
@@ -22,6 +28,33 @@ export class PlaylistHookComponent implements OnInit {
   }
 
   ngOnInit(): void {
+        // Clear the fields
+        this.validateForm = this.fb.group({
+          playlistName: [null, [Validators.required]],
+        });
+  }
+
+  playlistNameChange(){
+    this.appStateService.playlistName = this.playlistName;
+  }
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  createPlaylist() {
+
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+
+    if (this.validateForm.valid) {
+
+    }
   }
 
 }
